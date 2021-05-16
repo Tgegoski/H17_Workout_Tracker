e9040be6164721547c595515ab48c026a4ccdf7e
Tammy Gegoski <tgegoski@gmail.com>
2021-05-16 17:55:01 -0400

    set up workout schema

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -3,19 +3,55 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => Date.now()
+  },
+
+  exercises: [
+  {
+    type: {
+    type: String,
+    trim: true
+  },
+
   name: {
     type: String,
-    trim: true,
-    required: "Enter a name for transaction"
+    trim: true
   },
-  value: {
+
+  duration: {
     type: Number,
-    required: "Enter an amount"
+    default: 0,
+    allowNull: true
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+
+  weight: {
+    type: Number,
+    default: 0,
+    allowNull: true
+  },
+
+  sets: {
+    type: Number,
+    default: 0,
+    allowNull: true
+  },
+
+  reps: {
+    type: Number,
+    default: 0,
+    allowNull: true
+  },
+  
+  distance: {
+    type: Number,
+    default: 0,
+    allowNull: true
+  },
+
+}
+  ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
