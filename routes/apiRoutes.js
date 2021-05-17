7821bbf85690763ebced27e8f97743f88ecd4d7a
Tammy Gegoski <tgegoski@gmail.com>
2021-05-16 22:47:33 -0400

    set up api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,28 +12,8 @@ router.post("/api/workout", ({ body }, res) => {
     });
 });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-db.workout.aggregate( [
+router.get("/api/workouts", (req, res) => {
+  db.workout.aggregate( [
     { 
         $addFields: {
             totalExercises: { $sum: "$exercises" },
@@ -42,8 +22,67 @@ db.workout.aggregate( [
     },
     {
         $addFields: { totalDuration: 
-            { $add: ["$totalExercises","$totalDuration","$totalWorkout"]}}
+            { $add: ["$totalExercises","$totalDuration","$totalWorkout"] }}
     }
 ])
+     
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    dbWorkout.findOneAndUpdate(
+      {
+         _id: req.params.id
+      },
+      { $inc: {
+        totalDuration: req.body.totalDuration
+      },
+      $push: { 
+        workouts: req.body
+      }
+    },
+    { 
+      new: true
+    }
+)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+})
+
+router.get("/api/workouts/range", (req, res) => {
+  dbWorkout.find({})
+  db.workout.aggregate( [
+    { 
+        $addFields: {
+            totalWorkout: { $sum: "$workouts" },
+            totalInRange: { $sum: "$inRange" }
+        }
+    },
+    {
+        $addFields: { totalInRange: 
+            { $add: ["$totalWorkouts","$totalInRange"] }}
+    }
+  ])
+}
+
+).sort({ "id": 1, "day": 1 })
+    .limit (7)
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+  });
 
 module.exports = router;
