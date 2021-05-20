4687f96b08e39a8e0d83ad133f297196635ae58e
Tammy Gegoski <tgegoski@gmail.com>
2021-05-19 22:53:13 -0400

    formatted

--- models/workout.js
+++ models/workout.js
@@ -5,53 +5,45 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
   day: {
     type: Date,
-    default: () => Date.now()
+    default: () => Date.now(),
   },
-
   exercises: [
-  {
-    type: {
-    type: String,
-    trim: true
-  },
-
-  name: {
-    type: String,
-    trim: true
-  },
-
-  duration: {
-    type: Number,
-    default: 0,
-    allowNull: true
-  },
-
-  weight: {
-    type: Number,
-    default: 0,
-    allowNull: true
-  },
-
-  sets: {
-    type: Number,
-    default: 0,
-    allowNull: true
-  },
-
-  reps: {
-    type: Number,
-    default: 0,
-    allowNull: true
-  },
-  
-  distance: {
-    type: Number,
-    default: 0,
-    allowNull: true
-  },
-
-}
-  ]
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        default: 0,
+        allowNull: true,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+        allowNull: true,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+        allowNull: true,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+        allowNull: true,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+        allowNull: true,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
